890796359faf1e9f5bc2e7d445f548d288613074
Prathamesh Powar <ppowar1910@gmail.com>
2024-05-21 22:38:18 +0530

    chore: Add URL shortening functionality and API endpoints

--- url-shortener/main.go
+++ url-shortener/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
 	"time"
 )
 
@@ -47,8 +49,51 @@ func getURL(id string) (URL, error) {
 	return url, nil
 }
 
+func RootPageURL(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Request received")
+}
+
+func ShortURLHandler(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		URL string `json:"url"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	shortURL_ := createURL(data.URL)
+	// fmt.Fprintf(w, "Short URL: %s", shortURL)
+
+	response := struct {
+		ShortURL string `json:"short_url"`
+	}{ShortURL: shortURL_}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func redirectURLHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/redirect/"):]
+	url, err := getURL(id)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusNotFound)
+	}
+	http.Redirect(w, r, url.OriginalURL, http.StatusFound)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
-	OriginalURL := "https://www.google.com"
-	fmt.Println(generateShortURL(OriginalURL))
+	// OriginalURL := "https://www.google.com"
+	// fmt.Println(generateShortURL(OriginalURL))
+
+	//register the handler function to handle all the requests to root "/" path
+	http.HandleFunc("/", RootPageURL)
+	http.HandleFunc("/shorten", ShortURLHandler)
+	http.HandleFunc("/redirect/", redirectURLHandler)
+	//start the http server on port 3000
+	fmt.Println("Starting server on port 3000")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Error starting server", err)
+	}
 }
